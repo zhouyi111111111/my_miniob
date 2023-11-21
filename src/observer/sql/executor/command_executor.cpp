a42f7850759a6f334464ae3c3506d27f08e8e94a
zhouyi111111111 <124766866+zhouyi111111111@users.noreply.github.com>
2023-11-21 23:49:06 +0800

    Update command_executor.cpp
    111

--- src/observer/sql/executor/command_executor.cpp
+++ src/observer/sql/executor/command_executor.cpp
@@ -13,18 +13,19 @@ See the Mulan PSL v2 for more details. */
 //
 
 #include "sql/executor/command_executor.h"
-#include "common/log/log.h"
 #include "event/sql_event.h"
+#include "sql/stmt/stmt.h"
 #include "sql/executor/create_index_executor.h"
 #include "sql/executor/create_table_executor.h"
 #include "sql/executor/desc_table_executor.h"
 #include "sql/executor/help_executor.h"
-#include "sql/executor/load_data_executor.h"
-#include "sql/executor/set_variable_executor.h"
 #include "sql/executor/show_tables_executor.h"
 #include "sql/executor/trx_begin_executor.h"
 #include "sql/executor/trx_end_executor.h"
-#include "sql/stmt/stmt.h"
+#include "sql/executor/set_variable_executor.h"
+#include "sql/executor/load_data_executor.h"
+#include "common/log/log.h"
+#include "sql/executor/drop_table_executor.h"
 
 RC CommandExecutor::execute(SQLStageEvent *sql_event)
 {
@@ -40,6 +41,11 @@ RC CommandExecutor::execute(SQLStageEvent *sql_event)
       CreateTableExecutor executor;
       return executor.execute(sql_event);
     } break;
+
+    case StmtType::DROP_TABLE: {
+      DropTableExecutor executor;
+      return executor.execute(sql_event);
+    }
 
     case StmtType::DESC_TABLE: {
       DescTableExecutor executor;
