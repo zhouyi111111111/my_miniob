8b0328101d14853fb9893264a548119f65d27dae
zhouyi111111111 <124766866+zhouyi111111111@users.noreply.github.com>
2023-11-21 23:53:38 +0800

    Update table.cpp
    3

--- src/observer/storage/table/table.cpp
+++ src/observer/storage/table/table.cpp
@@ -525,15 +525,6 @@ RC Table::destroy(const char* dir){
       return RC::FILE_NOT_EXIST;          // 这里也有问题, 随便返回了一个RC
   } 
 
-  // 删除表实现text字段的数据文件（后续实现了text case时需要考虑，最开始可以不考虑这个逻辑）
-  // 所以暂且注释, 且有问题
-
-  // std::string text_data_file = std::string(dir) + "/" + name() + TABLETE;
-  // if(unlink(text_data_file.c_str()) != 0) { // 删除表实现text字段的数据文件（后续实现了text case时需要考虑，最开始可以不考虑这个逻辑）
-  //     LOG_ERROR("Failed to remove text data file=%s, errno=%d", text_data_file.c_str(), errno);
-  //     return RC::GENERIC_ERROR;
-  // }
-
    const int index_num = table_meta_.index_num();
     for (int i = 0; i < index_num; i++) {  // 清理所有的索引相关文件数据与索引元数据
         ((BplusTreeIndex*)indexes_[i])->close();
